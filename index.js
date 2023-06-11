0695b904f7d68d31331116d77d4352550408625e
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-11 01:08:45 -0400

    expected simple output cat little completed and not completed

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 function newTask(title, description) {
-  return {
-    title,
-    description,
+  const task = {
+    title: title,
+    description: description,
     complete: false,
 
-    logState() {
+    logState: function() {
       console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
     },
 
-    markCompleted() {
+    markCompleted: function() {
       this.complete = true;
     }
   };
+  return task;
 }
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
-console.log(tasks);
 
 task1.logState(); // Clean Cat Litter has not been completed
 task1.markCompleted();
 task1.logState(); // Clean Cat Litter has been completed
-
-console.log(tasks);
