895619f5c2d1a468b40650070d3469d627ef06a3
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-12 14:55:49 -0400

    added more tasks, refactored from original app to use objects to rep tasks and converted functions into methods of the task obj.  creates task objects then we call  method to log their state. after marking a task complete, we call  to verify their changed state for task1 at least.

--- index.js
+++ index.js
@@ -21,7 +21,16 @@ function newTask(title, description) {
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
+const task3 = newTask("Walk the Dog", "Go for a 20-minute run");
+const task4 = newTask("Buy Groceries", "Get milk, eggs, and bread");
 
 task1.logState(); // Clean Cat Litter has not been completed
+
 task1.markCompleted();
+task4.markCompleted();
+
 task1.logState(); // Clean Cat Litter has been completed
+task2.logState(); // Do Laundry has not been completed
+task3.logState(); // Walk the Dog has not been completed
+task4.logState(); // Buy Groceries has been completed
+
